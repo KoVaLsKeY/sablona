d7ac3bd97068179f9b8ab46b055ed99c1a44f627
KoValsKeY <vladyslav.kovalov@student.ukf.sk>
2025-03-30 16:47:46 +0200

    Vyprav chyby

--- php/index.php
+++ php/index.php
@@ -62,14 +62,7 @@
 <section class="container">
     <div class="row">
         <div class="col-100 text-center">
-            <?php
-            $hour = date('H');
-            if ($hour < 12) {
-                echo "<h3>DObre rabo</h3>";
-            } elseif ($hour <18 ) {
-                echo "<h3>Dobry den</h3>";
-            }
-            ?>
+           
         </div>
     </div>
 
