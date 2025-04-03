edfbf953c5f8e15045c8eb7c63de64a655074f4a
KoValsKeY <vladyslav.kovalov@student.ukf.sk>
2025-04-03 23:16:39 +0200

    Added slider with json file
    Also, i've got a problem here. For unknown reason, I got not 3 slides, but 6...

--- php/functions.php
+++ php/functions.php
@@ -1,8 +1,28 @@
 <?php 
-function pridajPozdrav(){
-            $hour = date('H');
-            if ($hour < 12) {
-                echo "<h3>DObre rano</h3>";
-            } elseif ($hour <18 ) {
-                echo "<h3>Dobry den</h3>";
-            }}
+function pridajPozdrav() {
+    $hour = date('H');
+    if ($hour < 12) {
+        echo "<h3>Dobré ráno</h3>";
+    } elseif ($hour < 18) {
+        echo "<h3>Dobrý deň</h3>";
+    } else {
+        echo "<h3>Dobrý večer</h3>";
+    }
+}
+
+function generateSlides($dir) {
+    $files = glob($dir . "/*.jpg");
+    $json = file_get_contents("data/datas.json"); 
+    $data = json_decode($json, true); 
+    $text = $data["text_banner"];
+
+    foreach ($files as $file) {
+        echo '<div class="slide fade">';
+        echo '<img src="' . $file . '">';
+        echo '<div class="slide-text">';
+        echo ($text[basename($file)]);
+        echo '</div>';
+        echo '</div>';
+    }
+}
+?>
--- php/index.php
+++ php/index.php
@@ -15,30 +15,16 @@
 $file_path1 = 'C:/xampp/htdocs/sablona/php/parts/header.php'; 
 if(!require($file_path1)){
   echo"Failed to include $file_path1";
-}?>
+}
+include_once "functions.php";
+?>
     
     <main>
       <section class="slides-container">
-        <div class="slide fade">
-          <img src="img/banner1.jpg">
-          <div class="slide-text">
-            Prvý nadpis
-          </div>
-        </div>
-      
-        <div class="slide fade">
-          <img src="img/banner2.jpg">
-          <div class="slide-text">
-            Druhý nadpis
-          </div>
-        </div>
-        
-        <div class="slide fade">
-          <img src="img/banner3.jpg">
-          <div class="slide-text">
-            Tretí nadpis
-          </div>
-        </div>
+      <?php 
+    include_once "functions.php";
+    generateSlides("img/banners"); 
+  ?>
         
         <a id="prev" class="prev">❮</a>
         <a id="next" class="next">❯</a>
@@ -59,7 +45,7 @@ if(!require($file_path1)){
           <div class="col-50">
             <p>Nulla dolore sit esse pariatur culpa sint nulla fugiat nulla ut. Sit adipisicing fugiat id consequat qui est. Nulla ea aliquip culpa nulla mollit do excepteur ex ullamco consequat tempor consectetur. Tempor et ea voluptate irure est magna magna reprehenderit qui non dolore. Consequat id ad cillum do qui. Proident ipsum enim eiusmod fugiat quis anim consequat ullamco mollit excepteur. Eiusmod eu ad irure anim velit laborum commodo nostrud pariatur id ea ad.</p>
             <p>Incididunt pariatur ipsum in culpa sit cillum consequat nostrud do. Esse commodo ad qui ea. Pariatur elit mollit deserunt aute do culpa eu veniam pariatur in adipisicing qui adipisicing irure. Est minim sint ipsum nulla eiusmod. Amet do officia consectetur labore. Id aute labore culpa aliquip. Adipisicing mollit excepteur Lorem quis elit ipsum irure quis occaecat qui consectetur ad irure dolor.</p>
-            <?php include "pridajPozdrav.php";?>
+           <?php pridajPozdrav();?>
 
           </div>
         </div>
@@ -68,7 +54,6 @@ if(!require($file_path1)){
 <section class="container">
     <div class="row">
         <div class="col-100 text-center">
-       <?php include "C:/xampp/htdocs/sablona/php/functions.php";?>
         </div>
     </div>
 
