7deefafec1abefe119b5c084a9c11be3fa526fe7
KoValsKeY <vladyslav.kovalov@student.ukf.sk>
2025-04-14 19:40:17 +0200

    pridanie časti "head" do suboru "head.php"

--- php/index.php
+++ php/index.php
@@ -1,14 +1,8 @@
 <?php ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/slider.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
+<?php include_once "parts/head.php"?>
+
 <body>
 <?php require 'C:/xampp/htdocs/sablona/php/parts/header.php';?>
 <?php 
--- php/qna.php
+++ php/qna.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html lang="sk">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moja stránka</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/accordion.css">
-    <link rel="stylesheet" href="css/banner.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
+<?php include_once "parts/head.php"?>
 <body>
 <?php include_once 'functions.php';
 $file_path1 = 'C:/xampp/htdocs/sablona/php/parts/header.php'; 
